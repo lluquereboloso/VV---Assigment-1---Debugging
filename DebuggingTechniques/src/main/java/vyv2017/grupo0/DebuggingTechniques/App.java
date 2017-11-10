547cbc43689d10fe3be67de050a096a80331908f
lluquereboloso <luis.luque.reboloso@alumnos.upm.es>
2017-11-10 11:10:41 +0100

    Creacion del proyecto maven Pilas

---
+++ DebuggingTechniques/src/main/java/vyv2017/grupo0/DebuggingTechniques/App.java
@@ -0,0 +1,13 @@
+package vyv2017.grupo0.DebuggingTechniques;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
