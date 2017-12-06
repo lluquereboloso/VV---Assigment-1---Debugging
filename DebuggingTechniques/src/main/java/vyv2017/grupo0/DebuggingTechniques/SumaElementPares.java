b0a6fbf12056b3142fb9a72d844f58344f92aa96
pmartindelcampo <pablo.martind@alumnos.upm.es>
2017-12-06 17:27:43 +0100

    Cambios hacia la cuarta hipotesis del MC

--- DebuggingTechniques/src/main/java/vyv2017/grupo0/DebuggingTechniques/SumaElementPares.java
+++ DebuggingTechniques/src/main/java/vyv2017/grupo0/DebuggingTechniques/SumaElementPares.java
@@ -7,12 +7,10 @@ public class SumaElementPares {
 		public int sumaElementPares (Pila D) throws PilaVacia {
 			int elem, suma=0, i=0;
 			if(!D.pilaVacia()){
-				while (i>10){
 					elem = D.desapilar();
 					suma = elem + sumaElementPares(D);
 					D.apilar(elem);
 					i=i+2;
-				}
 			}
 			return suma;
 			
